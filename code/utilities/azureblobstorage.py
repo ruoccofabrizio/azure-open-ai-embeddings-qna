af03a03c50f83a11a25d04d4a7f4606c44e54c6e
Ignacio Floristan <38979090+ignaciofls@users.noreply.github.com>
2023-01-20 08:46:08 +0400

    Update azureblobstorage.py

--- code/utilities/azureblobstorage.py
+++ code/utilities/azureblobstorage.py
@@ -49,3 +49,23 @@ def get_all_files():
             file['converted_path'] = converted_files[converted_filename]
     
     return files
+
+def process_all_within_blob(translate_enabled):
+    account_name = os.environ['BLOB_ACCOUNT_NAME']
+    account_key = os.environ['BLOB_ACCOUNT_KEY']
+    container_name = os.environ['BLOB_CONTAINER_NAME']
+    connect_str = f"DefaultEndpointsProtocol=https;AccountName={account_name};AccountKey={account_key};EndpointSuffix=core.windows.net"
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)    
+    container_client = blob_service_client.get_container_client(container_name)
+    blob_list = container_client.list_blobs()
+    sas = generate_container_sas(account_name, container_name,account_key=account_key,  permission="r", expiry=datetime.utcnow() + timedelta(hours=1))  
+    for blob in blob_list:
+        url = f"https://{account_name}.blob.core.windows.net/{container_name}/{blob.name}"
+        blob_sas=url+'?'+sas
+        # TO DO: check if already converted (in folder converted)
+        if blob.name.endswith('.pdf') or blob.name.endswith('jpeg') or blob.name.endswith('jpg') or blob.name.endswith('png'):
+            print('Processing sas url: ', blob_sas)
+            text=analyze_ocr(blob_sas, translate_enabled)
+            embeddings = chunk_and_embed(text)
+            redisembeddings.set_document(embeddings)
+            print('Embeddings added to redis for: ', blob.name)
