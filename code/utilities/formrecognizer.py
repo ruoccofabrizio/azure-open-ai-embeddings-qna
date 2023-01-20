6fef5adff1f2280dbc6c9fd831421d466c98ac2b
Ignacio Floristan <38979090+ignaciofls@users.noreply.github.com>
2023-01-20 10:55:39 +0400

    Update formrecognizer.py

--- code/utilities/formrecognizer.py
+++ code/utilities/formrecognizer.py
@@ -18,9 +18,7 @@ def analyze_ocr(formUrl, translate_enabled=False):
         content_en = results
     return content_en
     
-#Translates text (if not english) -- Possible improvement: checkbox to let user decide if they want to translate
 def translate(text):
-    # Detect if it is not english
     endpoint_detect = os.environ['TRANSLATE_ENDPOINT'] + "/detect?api-version=3.0"
     endpoint_detect
     print ('Endpoint is ', endpoint_detect)
@@ -36,8 +34,6 @@ def translate(text):
     request = requests.post(endpoint_detect, params=params, headers=headers, json=body)
     response = request.json()
     if response[0]['language'] != 'en':
-        # Translate to english
-
         endpoint_translate = os.environ['TRANSLATE_ENDPOINT'] + "/translate?api-version=3.0"
         params = urllib.parse.urlencode({
             'api-version': '3.0',
