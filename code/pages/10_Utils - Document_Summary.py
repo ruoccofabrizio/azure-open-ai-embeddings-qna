89db2d209e12e9f25bfa998a9f28c058a21db904
Ignacio Floristan <38979090+ignaciofls@users.noreply.github.com>
2023-01-25 19:42:49 +0400

    Layout changes and bring your own prompt

--- code/pages/10_Utils - Document_Summary.py
+++ code/pages/10_Utils - Document_Summary.py
@@ -4,10 +4,6 @@ import pandas as pd
 from utilities import utils
 import os
 
-def summarize():
-    _, response = utils.get_completion(get_prompt(), max_tokens=400, model='text-davinci-002')
-    st.session_state['summary'] = response['choices'][0]['text'].encode().decode()
-
 def clear_summary():
     st.session_state['summary'] = ""
 
@@ -21,8 +17,20 @@ def get_prompt():
         prompt = "Summarize the following text into bullet points:\n\n{}\n\nSummary:".format(text)
     elif summary_type == "Explain it to a second grader":
         prompt = "Explain the following text to a second grader:\n\n{}\n\nSummary:".format(text)
-
     return prompt
+
+def get_custom_prompt():
+    customtext = st.session_state['customtext']
+    customprompt = "{}".format(customtext)
+    return customprompt
+
+def summarize():
+    _, response = utils.get_completion(get_prompt(), max_tokens=500, model='davinci-002')
+    st.session_state['summary'] = response['choices'][0]['text'].encode().decode()
+
+def customcompletion():
+    _, response = utils.get_completion(get_custom_prompt(), max_tokens=500, model='davinci-002')
+    st.session_state['result'] = response['choices'][0]['text'].encode().decode()
 
 try:
     # Set page layout to wide screen and menu item
@@ -36,6 +44,18 @@ try:
     }
     st.set_page_config(layout="wide", menu_items=menu_items)
 
+    st.markdown("## Bring your own prompt")
+
+    # displaying a box for a custom prompt
+    st.session_state['customtext'] = st.text_area(label="Prompt",value='Legal clause: The Company and the Founders will provide the Investors with customary representations and warranties examples of which are set out in Appendix 4 and the Founders will provide the Investors with customary non-competition, non-solicitation and confidentiality undertakings.\n \n Plain English: The company and its founders will provide the usual assurances and guarantees on facts about the business. The founders will also agree not to work for competitors, poach employees or customers when they leave the company, and respect confidentiality. \n \n Legal clause: In the event of an initial public offering of the Companys shares on a US stock \n exchange the Investors shall be entitled to registration rights customary in transactions of this type (including two demand rights and unlimited shelf and piggy-back rights), with the expenses paid by the Company. \n \n Plain English: If the Company does an IPO in the USA, investors have the usual rights to include \n their shares in the public offering and the costs of doing this will be covered by the Company. \n \n Legal clause: Upon liquidation of the Company, the Series A Shareholders will receive in preference to all other shareholders an amount in respect of each Series A Share equal to one times the Original Issue Price (the "Liquidation Preference"), plus all accrued but unpaid dividends. To the extent that the Company has assets remaining after the distribution of that amount, the Series A Shareholders will participate with the holders of Ordinary Shares pro rata to the number of shares held on an as converted basis. \n \n Plain English:', height=400)
+    st.button(label="Test with your own prompt", on_click=customcompletion)
+    # displaying the summar
+    result = ""
+    if 'result' in st.session_state:
+        result = st.session_state['result']
+    st.text_area(label="OpenAI result", value=result, height=200)
+
+    st.markdown("## Summarization")
     # radio buttons for summary type
     summary_type = st.radio(
         "Select a type of summarization",
@@ -43,7 +63,7 @@ try:
         key="visibility"
     )
     # text area for user to input text
-    st.session_state['text'] = st.text_area(label="Enter some text to summarize", height=400)
+    st.session_state['text'] = st.text_area(label="Enter some text to summarize",value='A neutron star is the collapsed core of a massive supergiant star, which had a total mass of between 10 and 25 solar masses, possibly more if the star was especially metal-rich.[1] Neutron stars are the smallest and densest stellar objects, excluding black holes and hypothetical white holes, quark stars, and strange stars.[2] Neutron stars have a radius on the order of 10 kilometres (6.2 mi) and a mass of about 1.4 solar masses.[3] They result from the supernova explosion of a massive star, combined with gravitational collapse, that compresses the core past white dwarf star density to that of atomic nuclei.', height=200)
     st.button(label="Summarize", on_click=summarize)
 
     # if summary doesn't exist in the state, make it an empty string
@@ -52,12 +72,10 @@ try:
         summary = st.session_state['summary']
 
     # displaying the summary
-    st.text_area(label="Summary", value=summary, height=200)
+    st.text_area(label="Summary result", value=summary, height=200)
     st.button(label="Clear summary", on_click=clear_summary)
 
-    # displaying the prompt that was used to generate the summary
-    st.text_area(label="Prompt",value=get_prompt(), height=400)
-    st.button(label="Summarize with updated prompt")
+
 
 except URLError as e:
     st.error(
