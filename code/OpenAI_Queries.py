cb876acbf94c5f273dea4d37af0879b9cded2079
Fabrizio Ruocco <fabrizio.ruocco@microsoft.com>
2023-02-03 10:38:59 +0100

    added translation for response

--- code/OpenAI_Queries.py
+++ code/OpenAI_Queries.py
@@ -4,10 +4,14 @@ load_dotenv()
 import streamlit as st
 from urllib.error import URLError
 import pandas as pd
-from utilities import utils
+from utilities import utils, translator
 import os
 
 df = utils.initialize(engine='davinci')
+
+@st.cache(suppress_st_warning=True)
+def get_languages():
+    return translator.get_available_languages()
 
 try:
 
@@ -39,18 +43,24 @@ try:
     }
     st.set_page_config(layout="wide", menu_items=menu_items)
 
+    # Get available languages for translation
+    available_languages = get_languages()
+
     col1, col2, col3 = st.columns([1,2,1])
     with col1:
         st.image(os.path.join('images','microsoft.png'))
 
-    col1, col2, col3 = st.columns([2,2,1])
+    col1, col2, col3 = st.columns([2,2,2])
     with col3:
-        model = st.selectbox(
-            "OpenAI GPT-3 Model",
-            (os.environ['OPENAI_ENGINES'].split(','))
-        )
-        st.tokens_response = st.slider("Tokens response length", 100, 500)
-        st.temperature = st.slider("Temperature", 0.0, 1.0, 0.0)
+        with st.expander("Settings"):
+            model = st.selectbox(
+                "OpenAI GPT-3 Model",
+                (os.environ['OPENAI_ENGINES'].split(','))
+            )
+            st.tokens_response = st.slider("Tokens response length", 100, 500)
+            st.temperature = st.slider("Temperature", 0.0, 1.0, 0.0)
+            st.selectbox("Language", [None] + list(available_languages.keys()), key='translation_language')
+
 
     question = st.text_input("OpenAI Semantic Answer", default_question)
 
@@ -68,6 +78,10 @@ try:
             with st.expander("Question and Answer Context"):
                 st.text(st.session_state['prompt'].encode().decode())
 
+    if st.session_state['translation_language'] is not None:
+        st.write(f"Translation to other languages, 翻译成其他语言, النص باللغة العربية")
+        st.write(f"{translator.translate(st.session_state['response']['choices'][0]['text'], available_languages[st.session_state['translation_language']])}")		
+		
 except URLError as e:
     st.error(
         """
--- code/utilities/translator.py
+++ code/utilities/translator.py
@@ -1,6 +1,6 @@
 import os, requests, urllib
 
-def translate(text):
+def translate(text, language='en'):
     endpoint_detect = os.environ['TRANSLATE_ENDPOINT'] + "/detect?api-version=3.0"
     headers = {
         'Ocp-Apim-Subscription-Key': os.environ['TRANSLATE_KEY'],
@@ -13,12 +13,12 @@ def translate(text):
     }]
     request = requests.post(endpoint_detect, params=params, headers=headers, json=body)
     response = request.json()
-    if response[0]['language'] != 'en':
+    if (response[0]['language'] != language):
         endpoint_translate = os.environ['TRANSLATE_ENDPOINT'] + "/translate?api-version=3.0"
         params = urllib.parse.urlencode({
             'api-version': '3.0',
             'from': response[0]['language'],
-            'to': 'en'
+            'to': language
         })
         body = [{
             'text': text
@@ -28,3 +28,12 @@ def translate(text):
         return response[0]['translations'][0]['text']
     else:
         return text
+    
+
+def get_available_languages():
+    r = requests.get("https://api.cognitive.microsofttranslator.com/languages?api-version=3.0&scope=translation")
+    # languages = sorted([{v['name']: k} for k,v in r.json()['translation'].items()], key=lambda x: list(x.keys())[0])
+    languages = {}
+    for k,v  in r.json()['translation'].items():
+        languages[v['name']] =  k
+    return languages
