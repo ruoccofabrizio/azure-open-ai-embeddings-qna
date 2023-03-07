9e63e347f13c51cd7166c6e39567c8a7b45a6519
Ignacio Floristan <38979090+ignaciofls@users.noreply.github.com>
2023-03-07 09:18:07 +0400

    Fixed timestamp, grouped every 10 transcripts, added direct link to videoplayer

--- code/utilities/videoindexer.py
+++ code/utilities/videoindexer.py
@@ -2,8 +2,6 @@ import pandas as pd
 import os, json, re, io, requests
 from os import path
 from utilities.utils import add_embeddings
-from utilities.azureblobstorage import upsert_blob_metadata
-from langchain.text_splitter import CharacterTextSplitter
 
 videoaccountid=os.getenv("VIDEO_ACCOUNT_ID")
 videolocation=os.getenv("VIDEO_LOCATION")
@@ -39,6 +37,29 @@ def getAADtoken(clientid, clientsecret, tenantid, subsid, rg, videoaccountname):
     response = requests.request("POST", url, headers=headers, data=data)
     return response.json()['accessToken']
 
+def getplayertoken(clientid, clientsecret, tenantid, subsid, rg, videoaccountname, scope, videoid=None):
+    url = 'https://login.microsoftonline.com/'+tenantid+'/oauth2/v2.0/token'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+        'client_id': clientid,
+        'grant_type': 'client_credentials',
+        'scope': 'https://management.azure.com/.default',
+        'client_secret': clientsecret
+    }
+    response = requests.request("POST", url, headers=headers, data=data)
+    bearertoken = response.json()['access_token']
+    
+    url = 'https://management.azure.com/subscriptions/'+ subsid + '/resourceGroups/' + rg + '/providers/Microsoft.VideoIndexer/accounts/' + videoaccountname + '/generateAccessToken?api-version=2022-07-20-preview'
+    data = "{\r\n\tpermissionType: \"Reader\",\r\n\tscope: \"Video\"\r\n, videoId: \""+videoid+"\"\r\n}"
+    headers = {
+        'Authorization': 'Bearer  '+ bearertoken,
+        'Content-type': 'application/json'
+    }
+    response = requests.request("POST", url, headers=headers, data=data)
+    return response.json()['accessToken']
+
 def getallvideos(token, location, accountid):
     url = f"https://api.videoindexer.ai/{location}/Accounts/{accountid}/Videos?accessToken={token}"
     response = requests.request("GET", url)
@@ -50,27 +71,28 @@ def getvideoindexerresults(token, location, accountid, allvideos):
     try:
         for ids in allvideos['results']:
             if (ids['state'] == 'Processed'):
-                videotext, timestart=getonevideo(token, location, accountid, ids['id'])
-                print('processing video and start time: ', ids['id']+ ' ' + str(timestart))
-                videotranscripts = videotranscripts.append({'videoName': ids['id'], 'transcript': videotext, 'time':timestart}, ignore_index=True)
-        #print('done processing all videos')
-        print('videotranscripts df: ', videotranscripts)
+                tempdf = pd.DataFrame(columns=['videoName', 'transcript','time'])
+                tempdf = getonevideo(token, location, accountid, ids['id'])
+                videotranscripts= tempdf.append(videotranscripts, ignore_index=True)
         return videotranscripts
     except Exception as e:
         print(e)
 
+
 def getonevideo(token, location, accountid, videoid):
     url = f"https://api.videoindexer.ai/{location}/Accounts/{accountid}/Videos/{videoid}/Index?accessToken={token}"
     response = requests.request("GET", url)
-    print('\n')
-    videotranscript = ''    
-    timeoffset = ''    
-    for transcript in response.json()['videos'][0]['insights']['transcript']:
+    newvideotranscripts = pd.DataFrame(columns=['videoName','transcript','time'])
+    timeoffset = ''
+    videotranscript = ''
+    for index, transcript in enumerate(response.json()['videos'][0]['insights']['transcript']): #response.json()['videos'][0]['insights']['transcript']:
         videotranscript = videotranscript + transcript['text'] + ' '
-        timeoffset = transcript['instances'][0]['adjustedStart']
-        print ('videotranscript is: ', videotranscript)
-        print ('timeoffset is: ', timeoffset)
-    return videotranscript, timeoffset
+        if index % 10 == 0 or index == (len(response.json()['videos'][0]['insights']['transcript'])-1):
+            timeoffset = transcript['instances'][0]['adjustedStart']
+            newvideotranscripts = newvideotranscripts.append({'videoName': str(videoid+'_'+str(index)), 'transcript': videotranscript, 'time':timeoffset}, ignore_index=True)
+            videotranscript = ''
+    return newvideotranscripts
+
 
 def ingestvideos():
     videotoken = getAADtoken(clientid, clientsecret, tenantid, subsid, rg, videoaccountname)
@@ -78,16 +100,44 @@ def ingestvideos():
     video_index = getvideoindexerresults(videotoken, videolocation, videoaccountid, result_all_videos)
     for index, row in video_index.iterrows():
         try:
-            splitter = CharacterTextSplitter(chunk_size=1024,chunk_overlap=0, separator=' ')
-            chunks = []
-            chunks = splitter.split_text(row['transcript'])
-            #print('transcript chunks are: ', chunks)
-            for index, chunk in enumerate(chunks):
-                name=row['videoName']+'_'+str(index)
-                time=row['time']
-                print ('time is: ',time )
-                #print ('videoName is: ',name )
-                add_embeddings(chunk, name, time, os.getenv('OPENAI_EMBEDDINGS_ENGINE_DOC', 'text-search-davinci-doc-001'))
+            add_embeddings(row['transcript'], row['videoName'], row['time'], os.getenv('OPENAI_EMBEDDINGS_ENGINE_DOC', 'text-search-davinci-doc-001'))
         except Exception as e:
             print(e)
     print ('All videos done')
+
+def getplayertoken(clientid, clientsecret, tenantid, subsid, rg, videoaccountname, videoid):
+    url = 'https://login.microsoftonline.com/'+tenantid+'/oauth2/v2.0/token'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+        'client_id': clientid,
+        'grant_type': 'client_credentials',
+        'scope': 'https://management.azure.com/.default',
+        'client_secret': clientsecret
+    }
+    response = requests.request("POST", url, headers=headers, data=data)
+    bearertoken = response.json()['access_token']
+    
+    url = 'https://management.azure.com/subscriptions/'+ subsid + '/resourceGroups/' + rg + '/providers/Microsoft.VideoIndexer/accounts/' + videoaccountname + '/generateAccessToken?api-version=2022-07-20-preview'
+    data={
+        'permissionType': 'Reader',
+        'scope': 'Video',
+        'videoId': ''
+    }
+    if videoid:
+        data['videoId']=videoid
+    headers = {
+        'Authorization': 'Bearer  '+ bearertoken,
+        'Content-type': 'application/json'
+    }
+    response = requests.request("POST", url, headers=headers, data=str(data))
+    return response.json()['accessToken']
+
+def createvideolink(videoid,timestamp):
+    videotoken = getplayertoken(clientid, clientsecret, tenantid, subsid, rg, videoaccountname, videoid)
+    timestamp = int(timestamp.split(':')[0])*3600 + int(timestamp.split(':')[1])*60 + int(timestamp.split(':')[2].split('.')[0]) + int(timestamp.split(':')[2].split('.')[1])/1000
+    timestamp = int(timestamp)
+    url= 'https://www.videoindexer.ai/embed/player/'+videoaccountid+'/'+videoid+'/?accessToken='+videotoken+'&locale=en&location='+videolocation+'&t='+str(timestamp)
+    print(url)
+    return url
