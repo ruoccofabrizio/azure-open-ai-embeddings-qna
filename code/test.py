3f0d0e679e4fb5df8cab4bf164252090795dd36f
Anna <ankubasi@microsoft.com>
2023-03-22 09:31:07 +0100

    .

--- code/test.py
+++ code/test.py
@@ -1,2 +1,16 @@
-print(", ".join(['ala ma kota','ala']))
+import os
+from redis import Redis
+from redis.commands.search.query import Query
+from redis.commands.search.indexDefinition import IndexDefinition, IndexType
+from redis.commands.search.field import VectorField, TagField, TextField
+from dotenv import load_dotenv
+load_dotenv()
 
+index_name = "embeddings-index"
+prompt_index_name = "prompt-index"
+
+redis_conn = Redis(host= os.environ.get('REDIS_ADDRESS','localhost'), port=6379, password=os.environ.get('REDIS_PASSWORD',None)) #api for Docker localhost for local execution
+print('connecting..')
+try:
+    print(redis_conn.ft(index_name).info())
+except Exception as e: print(e)
