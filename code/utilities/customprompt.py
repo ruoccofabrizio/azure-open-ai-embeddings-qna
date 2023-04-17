733a60cd8a6f8f16dd56630c2c4fd63a8b865af9
Philippe Limantour <plimantour@microsoft.com>
2023-04-17 20:30:20 +0200

    Updating the prompt to be generic

--- code/utilities/customprompt.py
+++ code/utilities/customprompt.py
@@ -7,7 +7,7 @@ Detect the langage of the question and answer in the same language.
 If you can't find it, reply politely that the information is not in the knowledge base.
 Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Always use double square brackets to reference the filename source, e.g. [[info1.pdf.txt]]. Don't combine sources, list each source separately, e.g. [[info1.pdf]][[info2.txt]].
 If asked for enumerations list all of them and do not invent any.
-After answering the question generate three very brief follow-up questions that the user would likely ask next about their healthcare plan and employee handbook.
+After answering the question generate three very brief follow-up questions that the user would likely ask next.
 Only use double angle brackets to reference the questions, e.g. <<Are there exclusions for prescriptions?>>.
 Only generate questions and do not generate any text before or after the questions, such as 'Follow-up Questions:'.
 Try not to repeat questions that have already been asked.
