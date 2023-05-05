912a64ec867ba34460ba023c0fab0c38c14c6af7
Wen Guo <129209906+wenshi1688@users.noreply.github.com>
2023-05-05 17:57:59 +0000

    New image

--- code/OpenAI_Queries.py
+++ code/OpenAI_Queries.py
@@ -134,7 +134,7 @@ try:
 
     col1, col2, col3 = st.columns([1,2,1])
     with col1:
-        st.image(os.path.join('images','microsoft.png'))
+        st.image(os.path.join('images','wtw.png'))
 
     col1, col2, col3 = st.columns([2,2,2])
     with col1:
